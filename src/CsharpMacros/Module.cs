c89f8e39632d106c863e5123960484e33e8234bf
guillerglez88 <guillerglez88@gmail.com>
2022-08-19 18:56:40 +0200

    hability to translate simple dict expression

--- src/CsharpMacros/Module.cs
+++ src/CsharpMacros/Module.cs
@@ -18,5 +18,6 @@ public static class Module
         Initialize<Values>();
         Initialize<Fn>();
         Initialize<Members>();
+        Initialize<Dict>();
     }
 }
---
+++ tests/CsharpMacros.UnitTests/DictTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+using static CsharpMacros.Exp;
+using static CsharpMacros.Dict;
+
+namespace CsharpMacros.UnitTests;
+
+public class DictTests
+{
+    public DictTests()
+    {
+        Module.InitializeAllModules();
+    }
+
+    [Fact]
+    public void CanTranslateDict()
+    {
+        var dict =
+            D("a", E("const", 1),
+              "b", E("const", 2),
+              "greet", E("const", "hello"));
+
+        var exp = TranslateDict(dict);
+
+        Assert.IsAssignableFrom<MemberInitExpression>(exp);
+    }
+}
